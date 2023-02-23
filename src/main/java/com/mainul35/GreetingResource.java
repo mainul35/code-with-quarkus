57fba8278fc75ad514fee7d6592340975dfea123
mainul35 <mainuls18@gmail.com>
2023-02-24 00:00:29 +0700

    Updated for demo

--- src/main/java/com/mainul35/GreetingResource.java
+++ src/main/java/com/mainul35/GreetingResource.java
@@ -1,16 +1,27 @@
 package com.mainul35;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import com.mainul35.model.User;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
-
     @GET
+    @Path("hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "This is a test endpoint";
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response saveData(User user) {
+        System.out.println(user.toString());
+        return Response.ok(user).build();
     }
 }
