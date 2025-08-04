f3654936189df287f660255638816d5e8c78bf36
syedmainul.hasan <syedmainul.hasan@rakuten.com>
2025-08-04 20:35:33 +0900

    updating repo structure

---
+++ greetings/src/main/java/com/mainul35/GreetingResource.java
@@ -0,0 +1,27 @@
+package com.mainul35;
+
+import com.mainul35.model.User;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.Map;
+
+@Path("/")
+public class GreetingResource {
+    @GET
+    @Path("hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "This is my test endpoint";
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response saveData(User user) {
+        System.out.println(user.toString());
+        return Response.ok(user).build();
+    }
+}
